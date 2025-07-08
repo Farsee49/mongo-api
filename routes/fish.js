6076436f34e417707b488b295043333f0281a2ca
Farsee49 <farsee37@gmail.com>
2025-07-08 14:35:56 -0500

    post route

--- routes/fish.js
+++ routes/fish.js
@@ -10,8 +10,14 @@ fishRouter.get('/', catchAsync(async (req, res) => {
     res.status(200).json(fish);
 }));
 
+// Create a new fish
 
-
+fishRouter.post('/', catchAsync(async (req, res) => {
+    const fish = new Fish(req.body);
+    await fish.save();
+    res.status(201).json(fish);
+    console.log(`Added fish: ${fish.species}`);
+}));
 
 
 module.exports = fishRouter;
